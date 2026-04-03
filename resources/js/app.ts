3b99351ccd853cc38ff2c32b35f564439f4d60ee
Martin Lever <levermartin555@gmail.com>
2026-04-03 11:04:07 +0200

    update: bootstrap dashboard vue

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -10,19 +10,13 @@ createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     layout: (name) => {
         switch (true) {
-            case name === 'Welcome':
+            case name === 'Dashboard':
                 return null;
-            case name.startsWith('auth/'):
-                return AuthLayout;
-            case name.startsWith('settings/'):
-                return [AppLayout, SettingsLayout];
             default:
-                return AppLayout;
+                return null;
         }
     },
-    progress: {
-        color: '#4B5563',
-    },
+
 });
 
 // This will set light / dark mode on page load...
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
 
-Route::inertia('/', 'Welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'Dashboard')->name('dashboard');
-});
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 require __DIR__.'/settings.php';
