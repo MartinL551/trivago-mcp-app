078b78165182d4390d3e0e4d3f7027091ed2b205
Martin Lever <levermartin555@gmail.com>
2026-05-15 11:11:02 +0200

    new: bootstrap other needed pages

---
+++ resources/js/pages/PastSearches.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        Past Searches
+    </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/results/{searchRequest}', [ResultsController::class, 'index'])->name('results');
 Route::get('/', [SearchController::class, 'index'])->name('Search');
+
+
+
 Route::post('/search', [SearchController::class, 'search'])->name('search');
 
 require __DIR__.'/settings.php';
