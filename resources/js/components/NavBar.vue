83ce95b8dacd8e2c5f019c2d48b6a4ffa8929727
Martin Lever <levermartin555@gmail.com>
2026-06-15 16:14:17 +0200

    update: move navbar to own component

---
+++ resources/js/components/NavBar.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+    import { usePage } from '@inertiajs/vue3';
+    import { Circle } from 'lucide-vue-next';
+    import { route } from 'ziggy-js';
+    import type { NavItem } from '@/types';
+
+
+
+    const page = usePage();
+
+    const navItems: Array<NavItem> = [
+        {
+            title: 'Search',
+            icon: Circle,
+            href: 'search.index',
+        },
+        {
+            title: 'Last Result',
+            href: 'results.index',
+        },
+        {
+            title: 'Wishlist',
+            icon: Circle,
+            href: 'wishlist.show',
+        },
+        {
+            title: 'Manage Account',
+            icon: Circle,
+            href: 'profile.edit',
+        },
+    ];
+</script>
+
+<template>
+    <header class="shadow">
+        <span class="font-bold">Travel App</span>
+        <span class="mx-2 font-bold">{{ page.props.auth.user.name }}</span>
+        <nav class="mx-auto max-w-7xl px-4 py-4">
+            <ul>
+                <li v-for="item in navItems" :key="item.href">
+                    <a
+                        :href="route(item.href)"
+                    >
+                        {{ item.title }}
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,47 +1,10 @@
 <script setup lang="ts">
-    import { usePage } from '@inertiajs/vue3'
-    import { route } from 'ziggy-js';
-
-    const page = usePage();
-
-    const navItems = [
-            {
-                label: 'Search',
-                route: 'search.index',
-            },
-            {
-                label: 'Last Result',
-                route: 'results.index',
-            },
-            {
-                label: 'Wishlist',
-                route: 'wishlist.show',
-            },
-            {
-                label: 'Manage Account',
-                route: 'profile.edit',
-            },
-        ];
+    import NavBar from '@/components/NavBar.vue';
 </script>
 
 <template>
-    <div class="min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white">
-        <header class="shadow">
-            <span class="font-bold">Travel App</span>
-            <span class="mx-2 font-bold">{{ page.props.auth.user.name }}</span>
-            <nav class="mx-auto max-w-7xl px-4 py-4">
-                <ul>
-                    <li v-for="item in navItems" :key="item.route">
-                        <a
-                            :href="route(item.route)"
-                        >
-                            {{ item.label }}
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-
+    <div class="min-h-screen text-[var(--text)] bg-[var(--bg)]">
+        <nav-bar />
         <main class="mx-auto max-w-7xl px-4 py-6">
             <slot />
         </main>
